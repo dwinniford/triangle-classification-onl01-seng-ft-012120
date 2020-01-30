14facf1d0aecfc065e212966068c376c37b34253
Dave Winniford <dwinniford@gmail.com>
2020-01-30 19:02:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,16 @@
 class Triangle
-  
+  attr_accessor :s1, :s2, :s3
+ 
   def initialize (s1, s2, s3)
-    
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
   end 
   
   def kind 
-  
+    if s1 == s2 and s1 == s3
+      :equilateral 
+    end 
   end 
   
   
