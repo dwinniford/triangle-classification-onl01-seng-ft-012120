0bacd46f2f5e3234bcb2064c1e912a2643f8eedf
Dave Winniford <dwinniford@gmail.com>
2020-01-30 19:11:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   def kind 
     if s1 == s2 and s1 == s3
       :equilateral
-    elsif !(s1 == s2) and s2 == s3
+    elsif (!(s1 == s2) and s2 == s3) or (!(s1 == s3) and s1 == s2) or (!(s2 == s3) and s1 == s3)
       :isosceles
     end 
   end 
